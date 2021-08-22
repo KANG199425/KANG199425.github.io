e726faca90646c5d819c4c9e9cafa11efec7726c
Your Name <you@example.com>
2021-08-22 16:30:13 +0800

    Site updated: 2021-08-22 16:30:10

--- dist/music.js
+++ dist/music.js
@@ -2,28 +2,28 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: true,
-    lrcType: 3,
+	lrcType: 3,
     audio: [
+	  {
+        name: "告白气球",
+        artist: '周杰伦',
+        url: 'https://www.ytmp3.cn/down/38584.mp3',
+        cover: 'https://img2.kuwo.cn/star/albumcover/500/64/39/3540704654.jpg',
+        lrc: '../lrc/gaobaiqiqiu.lrc'
+      },
       {
-        name: "beautiful mistakes",
-        artist: 'maroon 5',
-        url: 'https://www.ytmp3.cn/down/74024.mp3',
-        cover: 'https://img1.kuwo.cn/star/albumcover/300/40/73/2271750349.jpg',
-        lrc: 'song-post/zaishuiyifang.lrc'
+        name: "贝加尔湖畔",
+        artist: '李健',
+        url: 'https://www.ytmp3.cn/down/44146.mp3',
+        cover: 'https://img3.kuwo.cn/star/albumcover/500/75/53/956274601.jpg',
+        lrc: '../lrc/beijiaerhupan.lrc'
       },
 	  {
-        name: "lemon",
-        artist: 'Tom',
-        url: 'https://www.ytmp3.cn/down/73653.mp3',
-        cover: 'https://star.kuwo.cn/star/starheads/300/72/69/1117895448.jpg',
-        lrc: 'song-post/zaishuiyifang.lrc'
-      },
-	  {
-        name: "huahai",
-        artist: 'JAY ZHOU',
+        name: "花海",
+        artist: '周杰伦',
         url: 'https://www.ytmp3.cn/down/73979.mp3',
         cover: 'https://img2.kuwo.cn/star/albumcover/300/36/9/13515874.jpg',
-        lrc: '../lrc/huahai11.lrc'
+        lrc: '../lrc/huahai.lrc'
       },
     ]
 });
